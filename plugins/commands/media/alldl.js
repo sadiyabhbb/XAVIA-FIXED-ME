97f3ec890fe7adb697531e789e2dbaceb13383c7
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-30 22:38:21 +0600

    Update and rename autodl.js to alldl.js

--- plugins/commands/media/alldl.js
+++ plugins/commands/media/alldl.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const config = {
-  name: "autodl",
+  name: "alldl",
   version: "2.1",
   author: "Dipto",
   credits: "Fixed by ChatGPT",
