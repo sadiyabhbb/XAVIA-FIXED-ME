db6543f0225568494542ccf42ac5630eff56eb55
sadiyabhbb <sadiyabhbb@proton.me>
2025-08-05 09:03:01 +0600

    Update listen.js

--- core/handlers/listen.js
+++ core/handlers/listen.js
@@ -3,70 +3,61 @@ import handleEvents from './events.js';
 import { handleDatabase } from './database.js';
 import logger from '../var/modules/logger.js';
 
-
 export default async function handleListen(listenerID) {
-    const { handleCommand, handleReaction, handleMessage, handleReply, handleUnsend, handleEvent } = await handleEvents();
+    const {
+        handleCommand,
+        handleReaction,
+        handleMessage,
+        handleReply,
+        handleUnsend,
+        handleEvent
+    } = await handleEvents();
+
     const eventlog_excluded = ["typ", "presence", "read_receipt"];
     const logger = global.modules.get('logger');
 
     function handleEventLog(event) {
-        const { LOG_LEVEL, timezone } = global.config;
+        const { LOG_LEVEL, timezone, selfListenEvent } = global.config;
+        const botID = global.botID || listenerID;
+
+        if (!selfListenEvent && event.senderID === botID) return;
 
         if (LOG_LEVEL == 0) return;
         if (eventlog_excluded.includes(event.type)) return;
+
         const { type, threadID, body, senderID } = event;
         if (LOG_LEVEL == 1) {
             let time = moment().tz(timezone).format('YYYY-MM-DD_HH:mm:ss');
-
             if (type == 'message' || type == 'message_reply') {
                 logger.custom(`${threadID} • ${senderID} • ${body ? body : 'Photo, video, sticker, etc.'}`, `${time}`);
+            } else {
+                logger.custom(`${threadID} • ${senderID} • ${type}`, `${time}`);
             }
-        } else if (LOG_LEVEL == 2) {
-            console.log(event);
         }
-
-        return;
     }
 
-    return (err, event) => {
-				if (global.listenerID != listenerID) return;
-				if (!event) {
-					logger.error(getLang("handlers.listen.accountError"))
-					process.exit(0);
-				}
-        if (global.maintain && !global.config.MODERATORS.some(e => e == event.senderID || e == event.userID)) return;
-        handleEventLog(event);
-        if (global.config.ALLOW_INBOX !== true && event.isGroup === false) return;
-        (async () => {
-            if (!eventlog_excluded.includes(event.type)) {
-                await handleDatabase({ ...event });
-            }
+    return async function (event) {
+        try {
+            handleEventLog(event);
+            await handleDatabase(event);
+
             switch (event.type) {
                 case "message":
                 case "message_reply":
-                    handleMessage({ ...event });
-                    handleReply({ ...event });
-                    handleCommand({ ...event });
-                    break;
-                case "message_reaction":
-                    handleReaction({ ...event });
-                    break;
-                case "message_unsend":
-                    handleUnsend({ ...event });
+                    await handleMessage(event);
                     break;
                 case "event":
-                case "change_thread_image":
-                    handleEvent({ ...event });
+                    await handleEvent(event);
                     break;
-                case "typ":
+                case "message_reaction":
+                    await handleReaction(event);
                     break;
-                case "presence":
-                    break;
-                case "read_receipt":
-                    break;
-                default:
+                case "message_unsend":
+                    await handleUnsend(event);
                     break;
             }
-        })();
-    }
+        } catch (e) {
+            console.error(e);
+        }
+    };
 }
